6de5c5c33a9af2aba17added1e83fba8a4df75f0
Akriti Suri <akritisuri96@gmail.com>
2020-12-01 18:26:44 -0800

    four

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -6,4 +6,10 @@ var tbody = d3.select("tbody");
 var button = d3.select("filter-btn"); 
 button.on("click", function() { 
     d3.event.preventDefault();
+
+    var datetimeElement = d3.select("#datetime"); 
+    var citynameElement = d3.select("#cityname"); 
+    var statenameElement = d3.select("#statename"); 
+    var countrynameElement = d3.select("#countryname"); 
+    var shapenameElement = d3.select("#shapename");
 }
