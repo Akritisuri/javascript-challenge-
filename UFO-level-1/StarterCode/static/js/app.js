8868ace775f671368f504937fa83b33bd9cc096e
Akriti Suri <akritisuri96@gmail.com>
2020-12-01 18:22:41 -0800

    three

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -2,3 +2,8 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+var button = d3.select("filter-btn"); 
+button.on("click", function() { 
+    d3.event.preventDefault();
+}
